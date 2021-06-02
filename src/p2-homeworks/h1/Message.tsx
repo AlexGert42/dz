53352e35714e5a004af9eb48d226fd3065863af6
MrAlexGert <alex.gert1812@gmail.com>
2021-06-02 14:28:50 +0300

    'task1'

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from './Message.module.css'
+
+type PropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+
+function Message({avatar, name, message, time}: PropsType) {
+    return (
+        <div className={style.message}>
+            <div className={style.message__inner}>
+                <img className={style.message__avatar} src={avatar} alt="avatar"/>
+                <div className={style.message__content}>
+                    <h2 className={style.message__title}>{name}</h2>
+                    <div className={style.message__text}>{message} <span>{time}</span></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message

ca799107e89c5457838d2640725075575939aa8f
MrAlexGert <alex.gert1812@gmail.com>
2021-06-06 22:08:05 +0300

    task5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
+const style = {
+    pageError: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '40px 50px',
+        justifyContent: 'space-between'
+    },
+    pageError404: {
+        fontSize: '250px',
+        fontWeight: 900,
+        fontFamily: "Comic Sans MS"
+    },
+    pageErrorText: {
+        fontSize: '150px',
+        fontWeight: 700,
+        fontFamily: "Comic Sans MS"
+    },
+    pageErrorSmail: {
+        fontSize: '120px',
+        fontFamily: "Comic Sans MS",
+        fontWeight: 900,
+    }
+}
+
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div style={style.pageError}>
+            <div>
+                <div style={style.pageError404}>404</div>
+                <div style={style.pageErrorText}>Page not found!</div>
+            </div>
+            <div>
+                <div style={style.pageErrorSmail}>¯\_(ツ)_/¯</div>
+            </div>
+
+
         </div>
     )
 }
